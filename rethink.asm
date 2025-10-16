1eb318e37d50fa1403819f8e9bf329be9e3e9cc5
TLDispenser <mechdispenser@gmail.com>
2025-10-16 13:40:47 +0000

    ???

--- rethink.asm
+++ rethink.asm
@@ -1,4 +1,6 @@
-; Header
+; So, essentially, we add a flag for the vblank.
+; Personally, I wouldn't, but the machine wouldn't shut up about it.
+
 .segment "HEADER"
     .byte   "NES", $1A
     .byte   2
@@ -6,15 +8,18 @@
     .byte   $01, $00
 
 .segment "ZEROPAGE"
+    ; Game variables
     gamestate:      .res 1
-
     playerx:        .res 1
     
+    ; Meta variables
     buttons1:       .res 1
-
+    vblankflag:     .res 1
+    
+    ; Consts
     PLAYERY         = $80
 
-    ; for the sake of sanity
+        ; for the sake of sanity
     BUTTON_LEFT     = %00000010
     BUTTON_RIGHT    = %00000001
     BUTTON_START    = %00010000
@@ -22,7 +27,6 @@
 .segment "STARTUP" ; Still don't know what this is for
 
 .segment "CODE"
-; Game time
 
 vblankwait:
     bit $2002
@@ -96,16 +100,28 @@ loadpalettes:
     cpx #$20
     bne @loop
 
-    lda $80
+; INIT VARS HERE
+    lda #$80
     sta playerx
+    lda #$01
+    sta gamestate ; I'll add stuff later
 
-; beginging of the loop
+; Maybe?
+    jsr vblankwait
+    lda #$01
+    sta vblankflag
 
-forever:
-    jmp forever
+; gaem
 
-vblank:
-    jsr readcontroller1
+gameloop:
+    lda vblankflag
+    cmp #$01
+    bne gameloop
+
+    ; actual game
+    ; ...after resetting the flag
+    lda #$00
+    sta vblankflag
 
     lda #%10010000
     sta $2000
@@ -113,56 +129,76 @@ vblank:
     sta $2001
     
     lda #$00
-    sta $2005 ;no scrolling (maybe later???)
+    sta $2005 ;no scrolling
     sta $2005 ;no scrolling (idk which is which)
 
-gameset:
-    ;add gamestate switching later
-    jmp play
-gamesetdone:
+    jsr readcontroller1
+
+    jsr checkgamestate
     jsr updatesprites
-    rti
+    
+    lda #$00
+    sta $2003
+    lda #$02
+    sta $4014
+jmp gameloop
 
-play:
-
-moveplayerleft:
-    lda buttons1
-    and BUTTON_LEFT
-    beq @done
-
-    lda playerx
-    sec
-    sbc #$01
-    sta playerx
-@done:
-
-moveplayerright:
-    lda buttons1
-    and BUTTON_RIGHT
-    beq @done
-
-    lda playerx
-    clc
-    adc #$01
-    sta playerx
-
-@done:
-    jmp gamesetdone
 
 ; -------- SUBROUTINES ---------
+checkgamestate:
+    lda gamestate
+    cmp #$00
+    beq titlescreen ; later
+    cmp #$01
+    beq ingame ; ya
+    
+    joever:
+        rts
+
+titlescreen: ; I'll fill this out later
+    
+    jmp joever
+
+ingame:
+
+    moveplayerleft:
+        lda buttons1
+        and #BUTTON_LEFT
+        beq @done
+
+        lda playerx
+        sec
+        sbc #$01
+        sta playerx
+    @done:
+
+    moveplayerright:
+        lda buttons1
+        and #BUTTON_RIGHT
+        beq @done
+
+        lda playerx
+        clc
+        adc #$01
+        sta playerx
+
+    @done:
+        jmp joever
+
 readcontroller1:
     lda #$01
     sta $4016
     lda #$00
     sta $4016
     ldx #$08
-@loop:
-    lda $4016
-    lsr A
-    rol buttons1
-    dex
-    bne @loop
-    rts
+
+    @loop:
+        lda $4016
+        lsr A
+        rol buttons1
+        dex
+        bne @loop
+        rts
 
 updatesprites:
     lda #PLAYERY
@@ -176,16 +212,27 @@ updatesprites:
 
     rts
 
-; palettes and the like
+vblank:
+    lda #$01
+    sta vblankflag
+    rti
 
+irq:        ; ?????
+    rti 
+
+; DATA AND STUFF
 palettes: ;for now, yoink smb's palettes
     .byte $22,$29,$1a,$0F,   $22,$36,$17,$0F,   $22,$30,$21,$0F,   $22,$27,$17,$0F  ; background palette data
     .byte $22,$16,$27,$18,   $22,$1A,$30,$27,   $22,$16,$30,$27,   $22,$0F,$36,$17  ; sprite palette data
 
+sprites:
+    ; 
+    .byte
+
 .segment "VECTORS"
     .word vblank
     .word reset
-    .word 0
+    .word irq
 
 .segment "CHARS"
     .incbin "shitchr.chr"
